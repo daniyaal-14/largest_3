b995924a4ce19c1d4eb6f8450b9b29c253ee6ff0
Daniyal Iqbal <iqbaldaniyal20@gmail.com>
2024-04-21 13:40:05 +0530

    21f3002977_Assignment8.py

--- 21f3002977_Assignment8.py
+++ 21f3002977_Assignment8.py
@@ -2,7 +2,7 @@ import streamlit as st
 def largest(n1, n2, n3):
     return max(n1, n2, n3)
 def main():
-    st.title("Largest Of 3")
+    st.title("Largest Of 3 Numbers")
 
     n1 = st.number_input("Enter your 1st number:", step=1)
     n2 = st.number_input("Enter the 2nd number:", step=1)
